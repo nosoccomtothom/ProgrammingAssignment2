1a57730fa0179f81bdf8ed59f1bbee6c2f3afb52
Esteve Serra Clavera <esteve.serra.clavera@e-club.info>
2014-06-18 04:08:42 -0400

    Typo

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
   ## Manage the entry matrix to be solved and the storring the solved matrix in cache
   ## Contains four subfunctions: set, get, setinverse and getinverse
   ## x contains the matrix to be inverted
